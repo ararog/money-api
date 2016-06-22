15e317e58f6122f73fab87bd909fdf42618d89f7
Rogerio Araujo <rogerio.araujo@gmail.com>
2016-06-22 14:26:39 -0300

    Some code formatting.

--- test/auth_controller_test.js
+++ test/auth_controller_test.js
@@ -1,17 +1,20 @@
 var restify = require('restify')
 var md5 = require('md5')
+var User = require('../models/user')
+
 // init the test client
 var client = restify.createJsonClient({
-    version: '*',
-    url: 'http://127.0.0.1:8080'
+  version: '*',
+  url: 'http://127.0.0.1:8080'
 });
 
 describe('auth', function() {
-
-    // Test #1
-    describe('perform authentication', function() {
-        it('should get a 200 response', function(done) {
-            client.post('/api/auth', { email: 'rogerio.araujo@gmail.com', password: md5('') }, function(err, req, res, data) {
+  describe('perform authentication', function() {
+    it('should get a 200 response', function(done) {
+      new User({email: 'rogerio.araujo@gmail.com', password: md5('123456')}})
+          .save()
+          .then(function(model) {
+            client.post('/api/auth', { email: 'rogerio.araujo@gmail.com', password: md5('123456') }, function(err, req, res, data) {
                 if (err) {
                     throw new Error(err);
                 }
@@ -22,6 +25,10 @@ describe('auth', function() {
                     done();
                 }
             });
-        });
+          })
+          .catch(function(err) {
+              res.send(500, { error: 'Could not insert data' })
+          });
     });
+  });
 });
